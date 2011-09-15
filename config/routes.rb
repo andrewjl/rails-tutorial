94059bb0013a6e86112aad3097f68c3933f26d74
Andrew Barinov <abarinov@gmail.com>
2011-09-15 05:46:41 -0700

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ Sample::Application.routes.draw do
   # get "pages/contact"
   # get "pages/about"
   
+  get "users/new"
+  
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
+  match '/signup',	:to => 'users#new'
   
   root :to => 'pages#home'
 
